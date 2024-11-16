318240a54aa476ae68d984f27c966b06eeeb66ec
Brian <briankgathui@gmail.com>
2024-11-16 16:31:44 +0300

    Add multer for image upload handling
    - Configured multer to store images in the public/images directory.
    - Added filename sanitization to handle spaces in uploaded file names.

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const multer = require("multer");
 
 // Middleware configuration
 server.use(middlewares);
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/images"); 
+    },
+    filename: function (req, file, cb) {
+        const sanitizedFilename = file.originalname.replace(/\s+/g, "-"); 
+        req.body.picture = sanitizedFilename; 
+        cb(null, sanitizedFilename); 
+    },
+});
+
+// Use multer to handle multipart form-data
+server.use(upload);
+
 
 // Use JSON-Server router
 server.use(router);
