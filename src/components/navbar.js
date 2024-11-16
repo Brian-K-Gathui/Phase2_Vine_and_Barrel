ffc9d972c18e5858ea7d128855d54201bcea4ad9
Brian <briankgathui@gmail.com>
2024-11-16 17:11:12 +0300

    Adding App.css file

---
+++ src/components/navbar.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+
+export function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-white box-shadow">
+            <div className="container">
+                <Link className="navbar-brand" to="/">
+                    <img src="/logo.png" alt="Vine & Barrel Logo" width="250" className="me-2" />
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar"
+                    aria-controls="offcanvasNavbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="offcanvas offcanvas-end"
+                    tabIndex="-1"
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                >
+                    <div className="offcanvas-header">
+                        <button
+                            type="button"
+                            className="btn-close text-reset"
+                            data-bs-dismiss="offcanvas"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact-us">
+                                    Contact Us
+                                </Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#!"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Admin
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><Link className="dropdown-item" to="/admin/products"> Inventory </Link></li>
+                                    <li><Link className="dropdown-item" to="/Profile"> Profile </Link></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><Link className="dropdown-item" to="/sighn-out" id='signout'> Sign Out </Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
---
+++ src/pages/dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export function Dashboard() {
+    return (
+        <div className="dashboard-container">
+            <div className="hero-section">
+                <h1>Welcome Back to Vine & Barrel</h1>
+                <p>
+                    Hello, Admin! We're excited to have you here. Take control of our wine inventory with ease – 
+                    browse, update, and manage our excellent wine collection effortlessly. Let's get started!
+                </p>
+                <Link to="/admin/products">
+                    <button className="btn" id="addWineButton">Manage Wine Inventory</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
