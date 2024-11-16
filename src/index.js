4b05e4562ddebbc172798eedc3db6efb421c2ec6
Brian <briankgathui@gmail.com>
2024-11-16 16:55:16 +0300

    Wrapping Routes in a footer and navbar

--- src/index.js
+++ src/index.js
@@ -1,46 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Main application component
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
+import { Dashboard } from "./pages/dashboard";
+
+import { NotFound } from "./pages/notfound";
+import { ContactUs } from "./pages/contactus";
+import { Profile } from "./pages/profile";
+import { Signout } from "./pages/signout";
+import ProductList from "./pages/admin/products/ProductList";
+import AddWine from "./pages/admin/products/CreateProduct";
+import EditWine from "./pages/admin/products/EditProduct";
+
 function App() {
   return (
-    // Wrapping the application in a BrowserRouter to enable routing functionality
     <BrowserRouter>
-      {/* Defining the routes for the application */}
+      <Navbar />
       <Routes>
-        {/* Fallback route for undefined paths, rendering a NotFound component */}
-        <Route path="*" element={<NotFound />} />
-        
-        {/* Route for the dashboard, which serves as the homepage */}
+      <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Dashboard />} />
-        
-        {/* Route for the user's profile page */}
         <Route path="/profile" element={<Profile />} />
-        
-        {/* Route for the contact us page */}
         <Route path="/contact-us" element={<ContactUs />} />
-        
-        {/* Route for the sign-out page */}
         <Route path="/sign-out" element={<Signout />} />
-        
-        {/* Route for viewing the list of products in the admin panel */}
         <Route path="/admin/products" element={<ProductList />} />
-        
-        {/* Route for creating a new wine product in the admin panel */}
         <Route path="/admin/products/create" element={<AddWine />} />
-        
-        {/* Route for editing an existing wine product using its ID */}
         <Route path="/admin/products/edit/:id" element={<EditWine />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
 
-// Creating the root DOM node to render the application
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Rendering the App component within React's StrictMode for highlighting potential issues
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
